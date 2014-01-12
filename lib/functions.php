3067f4ffba09e7adc0bf73cb1f1713a8933d53e8
man4toman <man4toman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-12 15:03:06 +0000

    new version
    git-svn-id: https://plugins.svn.wordpress.org/wp-parsidate/tags/1.0@837176 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/functions.php
@@ -0,0 +1,63 @@
+<?php
+
+function php_date_default_timezone_set($GMT) 
+{ 
+    $timezones = array( 
+    '-12:00'=>'Pacific/Kwajalein', 
+    '-11:00'=>'Pacific/Samoa', 
+    '-10:00'=>'Pacific/Honolulu', 
+    '-09:00'=>'America/Juneau', 
+    '-08:00'=>'America/Los_Angeles', 
+    '-07:00'=>'America/Denver', 
+    '-06:00'=>'America/Mexico_City', 
+    '-05:00'=>'America/New_York', 
+    '-04:00'=>'America/Caracas', 
+    '-03:30'=>'America/St_Johns', 
+    '-03:00'=>'America/Argentina/Buenos_Aires', 
+    '-02:00'=>'Atlantic/Azores',
+    '-01:00'=>'Atlantic/Azores', 
+    '+00:00'=>'Europe/London', 
+    '+01:00'=>'Europe/Paris', 
+    '+02:00'=>'Europe/Helsinki',
+    '+03:00'=>'Europe/Moscow',
+    '+03:30'=>'Asia/Tehran',
+    '+04:00'=>'Asia/Baku',
+    '+04:30'=>'Asia/Kabul',
+    '+05:00'=>'Asia/Karachi',
+    '+05:30'=>'Asia/Calcutta',
+    '+06:00'=>'Asia/Colombo',
+    '+07:00'=>'Asia/Bangkok',
+    '+08:00'=>'Asia/Singapore',
+    '+09:00'=>'Asia/Tokyo',
+    '+09:00'=>'Australia/Darwin', 
+    '+10:00'=>'Pacific/Guam', 
+    '+11:00'=>'Asia/Magadan', 
+    '+12:00'=>'Asia/Kamchatka');
+    return  $timezones[$GMT];
+}
+
+function per_number($number) 
+{
+    return str_replace(range(0,9),array('۰','۱','۲','۳','۴','۵','۶','۷','۸','۹'),$number);
+}
+
+function eng_number($number) 
+{
+    return str_replace(array('۰','۱','۲','۳','۴','۵','۶','۷','۸','۹'),range(0,9),$number);
+}
+
+function persian_number($content) 
+{
+    return(isset($content[1])?str_replace(range(0,9),array('۰','۱','۲','۳','۴','۵','۶','۷','۸','۹'),$content[1]):$content[0]);
+}
+
+function fixnumber($content) 
+{
+	return preg_replace_callback( '/(?:&#\d{2,4};)|(?:[0]?[a-z][\x20-\x3B=\x3F-\x7F]*)|(\d+[\.\d]*)|<\s*[^>]+>/i','persian_number',$content);
+}
+
+function fixarabic($content)
+{
+    return str_replace(array('ي','ك','٤','٥','٦','ة','ئ'),array('ی','ک','۴','۵','۶','ه','ی'),$content);   
+}
+?>
---
+++ readme.txt
@@ -0,0 +1,42 @@
+﻿=== wp-parsidate ===
+Contributors: lord_viper, man4toman
+Donate link: http://forum.wp-parsi.com/
+Tags: shamsi, wp-parsi, wpparsi, persian, parsi, farsi, jalali, date, calendar, i18n, l10n, Iran, Iranian, parsidate, rtl
+Requires at least: 3.6
+Tested up to: 3.8
+Stable tag: 1.0.0
+
+Persian package builder for WordPress, Full RTL and Shamsi(Jalali) date
+
+== Description ==
+
+Persian package builder for WordPress, Full RTL and Shamsi(Jalali) date. It's easy to use and powerful and fully featured.
+
+List of some features:
+
+* Shamsi(Jalali) date in Posts, comments, pages, archives, search, categories
+* Shamsi(Jalali) date in Permalinks
+* Shamsi(Jalali) date in admin sections such as posts lists, comments lists, pages lists
+* Shamsi(Jalali) date in post quick edit, comment quick edit, page quick edit of admin panel
+* Shamsi(Jalali) archive widget
+* RTL and fixed tinymce editor
+* Poweful and fast function for fixing Arabic (ي , ك) to Persian (ی , ک)
+* Poweful and fast function for Persian numbers
+
+
+== Installation ==
+
+1. Upload plugin folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. To use the widget, go to 'Widgets' and choose 'بایگانی تاریخ خورشیدی'
+
+== Frequently Asked Questions ==
+
+== Screenshots ==
+
+1. Persian package builder admin page
+2. Widget of 'بایگانی تاریخ خورشیدی'
+
+== Changelog ==
+= 1.0.0 =
+* Hello world...
