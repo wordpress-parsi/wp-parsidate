a21407691559d9eef0236ecfce6dc69b25960239
parselearn <parselearn@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-04 23:11:11 +0000

    version 2.0.0.alpha
    git-svn-id: https://plugins.svn.wordpress.org/wp-parsidate/trunk@1038499 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/plugins/woocommerce.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Makes WooCommerce compatible with WP-Parsidate plugin
+ *
+ * @package                 WP-Parsidate
+ * @subpackage              Plugins/WooCommerce
+ * @author                  Ehsaan
+ * @author                  Farhan Nisi
+ */
+
+class WPP_WooCommerce {
+    public static $instance = null;
+
+    /**
+     * Returns an instance of class
+     *
+     * @return          WPP_WooCommerce
+     */
+    public static function getInstance() {
+        if ( self::$instance == null )
+            self::$instance = new WPP_WooCommerce();
+
+        return self::$instance;
+    }
+
+    /**
+     * Hooks required tags
+     */
+    private function __construct() {
+        global $wpp_settings;
+        add_filter( 'wpp_plugins_compability_settings', array( $this, 'add_settings' ) );
+
+        if ( isset( $wpp_settings['woo_per_price'] ) && $wpp_settings['woo_per_price'] != 'disable' ) {
+            add_filter( 'woocommerce_sale_price_html', 'per_number' );
+            add_filter( 'woocommerce_price_html', 'per_number' );
+        }
+    }
+
+    /**
+     * Adds settings for toggle fixing
+     *
+     * @param           array $old_settings Old settings
+     * @return          array New settings
+     */
+    public function add_settings( $old_settings ) {
+        $options = array(
+            'enable'		=>	__( 'Enable', 'wp-parsidate' ),
+            'disable'		=>	__( 'Disable', 'wp-parsidate' )
+        );
+        $settings = array(
+            'woocommerce'       =>  array(
+                'id'            =>  'woocommerce',
+                'name'          =>  __( 'WooCommerce', 'wp-parsidate' ),
+                'type'          =>  'header'
+            ),
+            'woo_per_price'     =>  array(
+                'id'            =>  'woo_per_price',
+                'name'          =>  __( 'Fix prices', 'wp-parsidate' ),
+                'type'          =>  'radio',
+                'options'       =>  $options,
+                'std'           =>  'disable'
+            )
+        );
+
+        return array_merge( $old_settings, $settings );
+    }
+}
+
+return WPP_WooCommerce::getInstance();
