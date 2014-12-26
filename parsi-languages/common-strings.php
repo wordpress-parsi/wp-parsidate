9ca07df7ad24a57260e247730a60bf7735ea4c63
parselearn <parselearn@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-26 12:58:44 +0000

    Update 2.0 stable
    git-svn-id: https://plugins.svn.wordpress.org/wp-parsidate/trunk@1054476 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ parsi-languages/common-strings.php
@@ -0,0 +1,4 @@
+<?php
+__( 'WP-Parsidate', 'wp-parsidate' );
+__( 'Persian package for WordPress, Adds full RTL and Shamsi (Jalali) support for: posts, comments, pages, archives, search, categories, permalinks and all admin sections and TinyMce editor, lists, quick editor. This package has Jalali archive widget.', 'wp-parsidate' );
+__( 'WP-Parsi Team', 'wpo' );
