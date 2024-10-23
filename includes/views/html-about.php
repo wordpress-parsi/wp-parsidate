e492c882e34aa1f2e9b2d62d2da59c108797a266
Morteza Geransayeh <man4toman@gmail.com>
2024-10-24 00:30:19 +0330

    Update html-about.php

--- includes/views/html-about.php
+++ includes/views/html-about.php
@@ -26,9 +26,8 @@ $team_members = array(
 	'john_doe'   => array(
 		'name'   => esc_html__( 'John Doe', 'wp-parsidate' ),
 		'role'   => esc_html__( 'CEO', 'wp-parsidate' ),
-		'avatar' => WP_PARSI_URL . 'assets/images/avatar.png',
+		'avatar' => 'https://avatars.githubusercontent.com/u/935397',
 		'social' => array(
-			'facebook'  => '#',
 			'x-twitter' => '#',
 			'linkedin'  => '#',
 		),
@@ -36,9 +35,44 @@ $team_members = array(
 	'jane_smith' => array(
 		'name'   => esc_html__( 'Jane Smith', 'wp-parsidate' ),
 		'role'   => esc_html__( 'CTO', 'wp-parsidate' ),
-		'avatar' => WP_PARSI_URL . 'assets/images/avatar.png',
+		'avatar' => 'https://avatars.githubusercontent.com/u/935397',
 		'social' => array(
-			'facebook'  => '#',
+			'x-twitter' => '#',
+			'linkedin'  => '#',
+		)
+	),
+	'john_doe2'   => array(
+		'name'   => esc_html__( 'John Doe', 'wp-parsidate' ),
+		'role'   => esc_html__( 'CEO', 'wp-parsidate' ),
+		'avatar' => 'https://avatars.githubusercontent.com/u/935397',
+		'social' => array(
+			'x-twitter' => '#',
+			'linkedin'  => '#',
+		),
+	),
+	'jane_smith2' => array(
+		'name'   => esc_html__( 'Jane Smith', 'wp-parsidate' ),
+		'role'   => esc_html__( 'CTO', 'wp-parsidate' ),
+		'avatar' => 'https://avatars.githubusercontent.com/u/935397',
+		'social' => array(
+			'x-twitter' => '#',
+			'linkedin'  => '#',
+		)
+	),
+	'john_do3e'   => array(
+		'name'   => esc_html__( 'John Doe', 'wp-parsidate' ),
+		'role'   => esc_html__( 'CEO', 'wp-parsidate' ),
+		'avatar' => 'https://avatars.githubusercontent.com/u/935397',
+		'social' => array(
+			'x-twitter' => '#',
+			'linkedin'  => '#',
+		),
+	),
+	'jane_smith3' => array(
+		'name'   => esc_html__( 'Jane Smith', 'wp-parsidate' ),
+		'role'   => esc_html__( 'CTO', 'wp-parsidate' ),
+		'avatar' => 'https://avatars.githubusercontent.com/u/935397',
+		'social' => array(
 			'x-twitter' => '#',
 			'linkedin'  => '#',
 		)
@@ -51,14 +85,13 @@ $social_accounts = array(
 	'telegram'  => '#',
 	'x-twitter' => '#',
 	'linkedin'  => '#',
-	'facebook'  => '#',
 	'github'    => '#',
 );
 ?>
-<section class="about-us">
+<section class="about-us wpp-team">
     <h1><?php esc_html_e( 'About Us', 'wp-parsidate' ); ?></h1>
-    <p></p>
-    <p></p>
+    <p>گسترش محبوبيت و روز افزون سيستم مديريت محتواي وردپرس در ايران و جهان اين امر را ايجاب كرده تا هر روز نياز بيشتري به وجود منابع كامل در اين زمينه احساس شود. گروه وردپرس پارسي در ۱ ارديبهشت ماه ۱۳۹۱ با همكاري افراد فعال و متخصص وردپرس در جهت  پشتيباني و رفع كمبود‌هايي كه در وردپس پارسي مشاهده مي‌شود و براي توسعه و پيشرفت هر چه بيشتر آن در ايران ايجاد گرديده و در اين راستا تلاش ميكند . وردپرس پارسي كاملا كاربر محور بوده و رضايت كاربر در آن مهمتر از هر چيزي است .</p>
+    <p> گروه وردپرس پارسی جهت رفع نیازهاو و حمایت و پشتیبانی کاربران، ارائه سرویس ها و خدمات نوین و همچنین توسعه هرچه بیشتر وردپرس در ایران تشکیل یافت. گروه وردپرس پارسی به صورت کاملاً جدا اما همراستا با سایت مرجع وردپرس فارسی فعالیت می کند.</p>
     <div class="follow-us">
         <h3><?php esc_html_e( 'Follow Us', 'wp-parsidate' ); ?></h3>
         <div class="social-links">
@@ -71,20 +104,33 @@ $social_accounts = array(
 
 <section class="wpp-team">
     <h1><?php esc_html_e( 'Our Team', 'wp-parsidate' ); ?></h1>
-    <div class="wpp-team-members">
-		<?php foreach ( $team_members as $member ) : ?>
-            <div class="wpp-team-member">
-                <img src="<?php echo esc_url( $member['avatar'] ); ?>" alt="<?php echo esc_attr( $member['name'] ); ?>" class="avatar" loading="lazy">
-                <h2><?php echo esc_html( $member['name'] ); ?></h2>
-                <h4><?php echo esc_html( $member['role'] ); ?></h4>
-                <div class="social-links">
-					<?php foreach ( $member['social'] as $platform => $url ) : ?>
-                        <a href="<?php echo esc_url( $url ); ?>" target="_blank"><img src="<?php echo esc_url( WP_PARSI_URL . "assets/svg/$platform-brands-solid.svg" ); ?>" alt="<?php echo esc_attr( ucfirst( $platform ) ); ?>"></a>
-					<?php endforeach; ?>
-                </div>
-            </div>
-		<?php endforeach; ?>
-    </div>
+
+	<div class="contributer-list">
+		<div class="contributer-list__track">
+			<div class="contributer-list__grid">
+
+				<?php foreach ( $team_members as $member ) : ?>
+				<div class="contributer-list-item">
+					<div div="" class="contributer-list-info">
+						<div div="" class="contributer-list-name"><?php echo esc_html( $member['name'] ); ?></div>
+						<div div="" class="contributer-list-title"><?php echo esc_html( $member['role'] ); ?></div>
+
+						<ul class="contributer-list-tags">
+							<?php foreach ( $member['social'] as $platform => $url ) : ?>
+							<li class="contributer-list-tag"><a href="<?php echo esc_url( $url ); ?>"><img src="<?php echo esc_url( WP_PARSI_URL . "assets/svg/$platform-brands-solid.svg" ); ?>" width="15" height="15"></a></li>
+							<?php endforeach; ?>
+						</ul>
+					</div>
+
+					<div div="" class="contributer-list-image">
+						<img alt="<?php echo esc_attr( $member['name'] ); ?>" width="300" height="300" src="<?php echo esc_url( $member['avatar'] ); ?>" loading="lazy">
+					</div>
+				</div>
+				<?php endforeach; ?>
+			</div>
+		</div>
+	</div>
+
 </section>
 
 <section class="wpp-contributors">
