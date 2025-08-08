c332c860ca5e754e8cf3337e5be43d09e7e53f4f
Mehrshad Darzi <mehrshad198@gmail.com>
2025-08-08 08:58:37 +0330

    Fix RankMath ISO Price WooCommerce #268

--- includes/plugins/rank-math.php
+++ includes/plugins/rank-math.php
@@ -11,6 +11,7 @@ if (!class_exists('WPP_Rank_Math')) {
             add_filter("rank_math/opengraph/facebook/article_published_time", [$this, 'convert_date_time']);
             add_filter("rank_math/opengraph/facebook/article_modified_time", [$this, 'convert_date_time']);
             add_filter("rank_math/schema/videoobject", [$this, 'fix_upload_date'], 30);
+            add_filter('rank_math/snippet/rich_snippet_product_entity', [$this, 'fix_price_currency'], 30);
         }
 
         public function convert($datetime)
@@ -32,6 +33,18 @@ if (!class_exists('WPP_Rank_Math')) {
             return $schema;
         }
 
+        public function fix_price_currency($entity)
+        {
+            if (isset($entity['offers']['priceCurrency']) and strtoupper($entity['offers']['priceCurrency']) == "IRT") {
+
+                $entity['offers']['priceCurrency'] = 'IRR';
+                if (!empty($entity['offers']['price']) and (float)$entity['offers']['price'] > 0) {
+                    $entity['offers']['price'] = apply_filters("wpp_rank_math_product_price", ($entity['offers']['price'] * 10), $entity);
+                }
+            }
+
+            return $entity;
+        }
     }
 
     $GLOBALS['WPP_Rank_Math'] = new WPP_Rank_Math();
