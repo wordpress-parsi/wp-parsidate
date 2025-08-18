f73fbde02d4abb221f7e995c626bfbb80d57b5b3
Mehrshad Darzi <mehrshad198@gmail.com>
2025-08-18 11:53:25 +0330

    add wpp_date_is helper function #277

--- includes/tools.php
+++ includes/tools.php
@@ -106,6 +106,52 @@ if (!function_exists('wpp_is_time_validate')) {
     }
 }
 
+if (!function_exists('wpp_date_is')) {
+    function wpp_date_is($dateString, $format = 'Y-m-d\TH:i:sP'): array
+    {
+        $default = [
+            'status' => false,
+            'type' => null,
+            'value' => ''
+        ];
+
+        $dateString = eng_number($dateString);
+        $dateParts = date_parse_from_format($format, $dateString);
+        if ($dateParts['error_count'] > 0 || $dateParts['warning_count'] > 0) {
+            return $default;
+        }
+
+        $year = $dateParts['year'];
+        $month = $dateParts['month'];
+        $day = $dateParts['day'];
+
+        if ($year > 1900) {
+            if (checkdate($month, $day, $year)) {
+
+                return [
+                    'status' => true,
+                    'type' => 'gregorian',
+                    'value' => $dateString
+                ];
+            }
+        } elseif ($year < 1500) {
+            if ($month >= 1 && $month <= 12 && $day >= 1 && $day <= 31) {
+                if ($month > 6 && $day > 30) {
+                    return $default;
+                } else {
+
+                    return [
+                        'status' => true,
+                        'type' => 'jalali',
+                        'value' => $dateString
+                    ];
+                }
+            }
+        }
+
+        return $default;
+    }
+}
 /*if ( wpp_is_active( 'disable_copy' ) ) {
 	if ( ! function_exists( 'wpp_disable_copy' ) ) {
 		function wpp_disable_copy() {
