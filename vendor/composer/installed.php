7c50ebe6401c3d849853dc67df73ec6ef8b271fe
Parsa Kafi <parselearn@gmail.com>
2025-10-06 02:12:23 +0330

    fix: Fatal error autoload.php require_once. Add vendor dir!

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wordpress-parsi/wp-parsidate',
+        'pretty_version' => '6.x-dev',
+        'version' => '6.9999999.9999999.9999999-dev',
+        'reference' => '7679a90c40b205a1c7c9e65710e4533211267fe8',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'enshrined/svg-sanitize' => array(
+            'pretty_version' => '0.21.0',
+            'version' => '0.21.0.0',
+            'reference' => '5e477468fac5c5ce933dce53af3e8e4e58dcccc9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../enshrined/svg-sanitize',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wordpress-parsi/wp-parsidate' => array(
+            'pretty_version' => '6.x-dev',
+            'version' => '6.9999999.9999999.9999999-dev',
+            'reference' => '7679a90c40b205a1c7c9e65710e4533211267fe8',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
