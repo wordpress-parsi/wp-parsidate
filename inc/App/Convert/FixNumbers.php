5ec0d27b64b26eb9d02359918b4f6fa00dfd69ec
Parsa Kafi <parselearn@gmail.com>
2026-06-26 03:50:28 +0330

    add: for safety convert numbers in post content add hybrid approach (DOM/regex) method VS regex only
    - Pipeline
    -- regex-protect fragile blocks
    -- DOM parse the rest
    -- convert only text nodes
    -- restore protected blocks exactly
    - Pros
    -- avoids regex touching JS/CSS/code
    -- avoids DOM mutating protected fragile blocks
    -- still safe for regular post HTML
    -- much more maintainable than giant regex
    - Cons
    -- slower than pure regex
    -- more code

--- inc/App/Convert/FixNumbers.php
+++ inc/App/Convert/FixNumbers.php
@@ -8,7 +8,7 @@
 
 namespace WPParsidate\App\Convert;
 
-use WPParsidate\Helper\Number;
+use WPParsidate\Helper\{Number, NumberConverter, WordPress};
 use WPParsidate\Settings\Settings;
 
 class FixNumbers {
@@ -26,7 +26,7 @@ class FixNumbers {
     }
 
     if ( Settings::get( 'conv_contents', false ) ) {
-      add_filter( 'the_content', [ $this, 'fixNumbersToPersian' ], 1000 );
+      add_filter( 'the_content', [ $this, 'fixPostContentNumbers' ], 1000 );
     }
 
     if ( Settings::get( 'conv_excerpt', false ) ) {
@@ -44,6 +44,68 @@ class FixNumbers {
     if ( Settings::get( 'conv_cats', false ) ) {
       add_filter( 'wp_list_categories', [ $this, 'fixNumbersToPersian' ], 1000 );
     }
+  }
+
+  /**
+   * Fix numbers in content (Post content with HTML) and convert them to Persian digits
+   *
+   * @param $content
+   *
+   * @return string
+   */
+  public function fixPostContentNumbers( $content ): string {
+    if ( ! self::shouldRunContentNumberConverter( $content ) ) {
+      return $content;
+    }
+
+    $converted = NumberConverter::convertContent( $content );
+
+    return apply_filters( 'wp_parsidate_fix_post_content_numbers', $converted, $content );
+  }
+
+  /**
+   * Decide whether filter should run in current request context.
+   *
+   * @param string $content
+   *
+   * @return bool
+   */
+  private static function shouldRunContentNumberConverter( string $content ): bool {
+    if ( $content === '' ) {
+      return false;
+    }
+
+    // No digits => no work.
+    if ( ! preg_match( '/[0-9]/', $content ) ) {
+      return false;
+    }
+
+    // No Persian/Arabic chars => skip.
+    // Remove if your site should convert numbers in all languages.
+    if ( ! preg_match( '/[\x{0600}-\x{06FF}\x{0750}-\x{077F}\x{08A0}-\x{08FF}]/u', $content ) ) {
+      return false;
+    }
+
+    // Avoid admin screens except AJAX if needed.
+    if ( is_admin() && ! wp_doing_ajax() ) {
+      return false;
+    }
+
+    // Usually don't touch feeds.
+    if ( WordPress::isFeed() ) {
+      return false;
+    }
+
+    // Optional: skip REST responses if you don't want transformed content there.
+    if ( defined( 'REST_REQUEST' ) && REST_REQUEST ) {
+      return false;
+    }
+
+    if ( is_preview() ) {
+      return false;
+    }
+
+    return true;
   }
 
   /**
