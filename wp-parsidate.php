9012c7b221d41a10ea7ed21f5898c4d9a2364586
Parsa Kafi <parselearn@gmail.com>
2026-06-29 01:38:13 +0330

    chore: update version to 6.1

--- wp-parsidate.php
+++ wp-parsidate.php
@@ -2,7 +2,7 @@
 
 /**
  * Plugin Name: WP-Parsidate
- * Version: 6.0
+ * Version: 6.1
  * Plugin URI: https://wp-parsi.com/support/
  * Description: Persian package for WordPress, Adds full RTL and Shamsi (Jalali) support for: posts, comments, pages, archives, search, categories, permalinks and all admin sections and TinyMce editor, lists, quick editor. This package has Jalali archive widget.
  * Author: WP-Parsi Team
