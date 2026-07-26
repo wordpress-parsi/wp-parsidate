92b0674c6cfb16989275ff98fabe2b876bb7f12d
Parsa Kafi <parselearn@gmail.com>
2026-07-26 16:18:44 +0330

    chore: Rename plugin assets

--- inc/Admin/AdminAssets.php
+++ inc/Admin/AdminAssets.php
@@ -22,25 +22,26 @@ class AdminAssets {
     //wp_enqueue_style( 'wp-color-picker' );
     //wp_enqueue_script( 'wp-color-picker' );
 
-    wp_enqueue_style( WP_PARSI_KEY_SLUG . '-admin-style',
-      Assets::url( 'css-admin/admin-style' . $debugName . '.css' ), false, $pluginVersion );
+    wp_enqueue_style( WP_PARSI_KEY_SLUG . '-plugin',
+      Assets::url( 'css-admin/plugin' . $debugName . '.css' ), false, $pluginVersion );
 
-    wp_enqueue_script( WP_PARSI_KEY_SLUG . '-admin',
-      Assets::url( 'js-admin/script.min.js' ),
+    wp_enqueue_script( WP_PARSI_KEY_SLUG . '-plugin',
+      Assets::url( 'js-admin/plugin.min.js' ),
       [
         'jquery',
         'jquery-ui-sortable',
       ], $pluginVersion, [ 'in_footer' => true ] );
 
-    wp_localize_script( WP_PARSI_KEY_SLUG . '-admin', WP_PARSI_KEY_CAP, array(
-      'ajaxUrl'            => admin_url( 'admin-ajax.php' ),
-      'ajaxNonce'          => Nonce::create(),
-      'pageRefreshedAfter' => apply_filters( 'wp_parsidate_settings_page_refreshed_after', 0 ),
-      'pageRefreshUrl'     => apply_filters( 'wp_parsidate_settings_page_refresh_url', null ),
-      'removeText'         => esc_html__( 'Remove', 'wp-parsidate' ),
-      'dtuConfirmDelete'   => esc_html__( 'Are you sure you want to delete this item(s)?', 'wp-parsidate' ),
-      'copyText'           => esc_html__( 'Click to copy this text.', 'wp-parsidate' ),
-    ) );
+    wp_localize_script( WP_PARSI_KEY_SLUG . '-plugin', WP_PARSI_KEY_CAP,
+      array(
+        'ajaxUrl'            => admin_url( 'admin-ajax.php' ),
+        'ajaxNonce'          => Nonce::create(),
+        'pageRefreshedAfter' => apply_filters( 'wp_parsidate_settings_page_refreshed_after', 0 ),
+        'pageRefreshUrl'     => apply_filters( 'wp_parsidate_settings_page_refresh_url', null ),
+        'removeText'         => esc_html__( 'Remove', 'wp-parsidate' ),
+        'dtuConfirmDelete'   => esc_html__( 'Are you sure you want to delete this item(s)?', 'wp-parsidate' ),
+        'copyText'           => esc_html__( 'Click to copy this text.', 'wp-parsidate' ),
+      ) );
   }
 
   public static function imageUrl( $path ): string {
